b90de29bf5e87bff643277a5824ea7de635bba3b
Lagawaga <kamronjahani@gmail.com>
2019-02-16 16:30:01 -0500

    Code: wrote hello world to test

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+var http = require('http');
+
+var server = http.createServer(function(request,response){
+	response.writeHead(200, {'Content-type': 'text/plain'});
+	response.end("Hello world");
+});
+
+server.listen(8000);
+
+console.log("Server is running...");
