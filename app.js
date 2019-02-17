c89fc1e74ea77728fb180434ceacb67229a2d6d7
Lagawaga <kamronjahani@gmail.com>
2019-02-16 19:45:47 -0500

    Code: Basic code layout

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-/**
- * 
- */
+/*
 
 var http = require('http');
 
 var server = http.createServer(function(request,response){
-	response.writeHead(200, {'Content-type': 'text/plain'});
-	response.end("Hello world");
+	response.writeHead(200, {'Content-type': 'text/html'});
+	response.write("<html><body><h1>");
+	response.end("Splurge");
 });
 
 server.listen(8000);
 
 console.log("Server is running...");
+
+*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.render('index');  
+})
+app.listen(3000, function () {
+  console.log('app listening on port 3000...')
+})
